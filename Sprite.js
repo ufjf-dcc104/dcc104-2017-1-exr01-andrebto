065507780737bb3f8146f05f2afd37b4bbf3f477
Andre Brito <andre.brito@ice.ufjf.br>
2017-03-29 19:20:24 -0300

    Adiciona fundo, life e colisoes

--- Sprite.js
+++ Sprite.js
@@ -5,15 +5,17 @@ function Sprite() {
   this.vy = 0;
   this.ax = 0;
   this.ay = 0;
+  this.w = 0;
+  this.h = 0;
   this.color = "blue";
 }
 
 
 Sprite.prototype.desenhar = function (ctx) {
   ctx.fillStyle = this.color;
-  ctx.fillRect(this.x, this.y, 15, 15);
+  ctx.fillRect(this.x, this.y, this.w, this.h);
   ctx.strokeStyle = this.color;
-  ctx.strokeRect(this.x, this.y, 15, 15);
+  ctx.strokeRect(this.x, this.y, this.w, this.h);
 }
 
 
@@ -22,4 +24,11 @@ Sprite.prototype.mover = function (dt) {
   this.vy = this.vy + (this.ay+60)*dt;
   this.x = this.x + this.vx*dt;
   this.y = this.y + this.vy*dt;
+};
+
+Sprite.prototype.collide = function (r2) {
+	if(((this.x+this.w < r2.x) || (this.x > r2.x+r2.w)) || ((this.y+this.h < r2.y) || (this.y > r2.y+r2.h)))
+		return false;
+	else
+		return true;
 };
